26aa4e1204728705b126823cd40071047a3545a7
cristian0707 <107206067+cristian0707@users.noreply.github.com>
2023-10-24 17:13:41 -0400

    null check fix

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -4,9 +4,9 @@ import { getSession } from 'next-auth/react'
 
 async function authRequestInterceptor(config: any) {
   config.headers.Accept = 'application/json';
-  const { user: data }: any = await getSession();
-  if (data['id_token']) {
-    config.headers['x-jwt'] = data['id_token'];
+  const  data: any = await getSession();
+  if (data?.user?.id_token) {
+    config.headers['x-jwt'] = data?.user?.id_token;
   }
   return config;
 }
