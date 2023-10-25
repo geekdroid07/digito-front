bfe84f1307e2b516d6b321b3b68373287709b838
cristian0707 <107206067+cristian0707@users.noreply.github.com>
2023-10-25 00:20:48 -0400

    some changes

--- src/pages/api/restricted.ts
+++ src/pages/api/restricted.ts
@@ -4,8 +4,7 @@ import { decode, getToken } from 'next-auth/jwt';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         const sessionToken = req.cookies['__Secure-next-auth.session-token'];
-        console.log({sessionToken});
-        
+
         const decoded = await decode({
             token: sessionToken as string,
             secret: process.env.NEXT_PUBLIC_SECRET
