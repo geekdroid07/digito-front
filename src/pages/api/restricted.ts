3ea4d08e186f178f18fc30ff1e0cb87f6d49b572
cristian0707 <107206067+cristian0707@users.noreply.github.com>
2023-10-24 23:11:47 -0400

    null check fix

--- src/pages/api/restricted.ts
+++ src/pages/api/restricted.ts
@@ -2,15 +2,20 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { decode } from 'next-auth/jwt';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const sessionToken = req.cookies['next-auth.session-token'];
+    try {
+        const sessionToken = req.cookies['next-auth.session-token'];
     
-    const decoded = await decode({
-        token: sessionToken as string,
-        secret: process.env.NEXT_PUBLIC_SECRET
-    });
-    if (decoded?.email) {
-        res.send({ok: true})
-    } else {
-        res.send({ok: false})
+        const decoded = await decode({
+            token: sessionToken as string,
+            secret: process.env.NEXT_PUBLIC_SECRET
+        });
+        if (decoded?.email) {
+            res.send({ok: true});
+        } else {
+            res.send({ok: false});
+        }
+    } catch (error) {
+        console.log(error);
+        throw error;
     }
 }
